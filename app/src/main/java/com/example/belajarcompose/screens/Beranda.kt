894b8a527316d021c809208a41b9a36c1250f77a
Safikka <mahdysyafiq160998@gmail.com>
2022-06-03 08:10:06 +0700

    [UPDATE] nambahin lazyrow

--- app/src/main/java/com/example/belajarcompose/screens/Beranda.kt
+++ app/src/main/java/com/example/belajarcompose/screens/Beranda.kt
@@ -2,30 +2,24 @@ package com.example.belajarcompose.screens
 
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.filled.Print
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.widget.ConstraintLayout
-import com.example.belajarcompose.R
 import com.example.belajarcompose.ui.theme.Delameta
 import com.example.belajarcompose.ui.theme.WhiteBox
 
@@ -70,18 +64,27 @@ fun BerandaScreen() {
                 Column(
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(20.dp)
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
+                    Text(
+                        text = "Hardware",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        textAlign = TextAlign.Center
+                    )
                     Spacer(modifier = Modifier.padding(10.dp))
-                    Row(
-                        modifier = Modifier
-                            .fillMaxSize(),
-                        verticalAlignment = Alignment.CenterVertically,
-                        horizontalArrangement = Arrangement.Center
-                    ) {
-                        CardButton("Printer")
-                        Spacer(modifier = Modifier.padding(10.dp))
-                        CardButton("Camera")
+                    LazyRow(
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ){
+                        item{
+                            CardButton(Judul = "Printer", Icon = Icons.Filled.Print)
+                            Spacer(modifier = Modifier.padding(10.dp))
+                            CardButton(Judul = "Camera", Icon = Icons.Filled.PhotoCamera)
+                            Spacer(modifier = Modifier.padding(10.dp))
+                            CardButton(Judul = "Mifare", Icon = Icons.Filled.CardGiftcard)
+                        }
                     }
                 }
             }
@@ -104,7 +107,7 @@ fun HeadingItem() {
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = "Sinau Hardware",
+            text = "Sinau POS",
             color = Color.White,
             fontWeight = FontWeight.Bold,
             fontSize = 20.sp
@@ -115,14 +118,16 @@ fun HeadingItem() {
 
 @Composable
 fun CardButton(
-    Judul:String
+    Judul:String,
+    Icon: ImageVector
 ) {
-    Box(
+    Card(
         modifier = Modifier
             .width(150.dp)
             .clip(RoundedCornerShape(16.dp))
             .background(Color.White)
-            .clickable {}
+            .clickable {},
+        elevation = 10.dp
     ){
         Column(
             Modifier
@@ -133,13 +138,8 @@ fun CardButton(
                 modifier = Modifier.fillMaxWidth(),
                 contentAlignment = Alignment.Center
             ){
-//                Image(
-//                    painter = painterResource(id = R.drawable.ic_red_rose_bouquet),
-//                    contentDescription = "",
-//                    modifier = Modifier.size(100.dp)
-//                )
                 Icon(
-                    imageVector = Icons.Filled.Print,
+                    imageVector = Icon,
                     contentDescription = "",
                     tint = Color.Black,
                     modifier = Modifier
